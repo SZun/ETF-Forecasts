e2e674ace6855a189622c6f6a0cd4bd17872c2a7
SZun <zunsamuel97@gmail.com>
2023-01-08 10:31:34 -0600

    commented plotting class

--- plotting.py
+++ plotting.py
@@ -8,10 +8,24 @@ from pathlib import Path
 class Plotter:
     
     """
-    
+        This plots dataframes using hvplot.pandas and seaborn
+        
+        @METHODS:
+        - __init__: Class constructor
+        - get_image_path: Returns path for image by title
+        - line: Plots and saves hvplot line plot
+        - bar: Plots and saves hvplot line bar
+        - heatmap: Plots and saves seaborn heatmap
+        
     """
     
     def __init__(self):
+        
+        """
+            Initializes Variables
+        """
+        
+        # Initializing class variables
         self.rot = 45
         self.grid = True
         self.height = 750
@@ -22,12 +36,24 @@ class Plotter:
         self.bgcolor = '#F5F5F5'
         
     def get_image_path(self, title): 
+        
+        """
+            Returns path for image by title
+        """
+        # Returning path for image
         return Path(f'./Resources/images/plots/{title.lower().replace(" ","_")}.png')
         
     def line(self, df, title, bgcolor='#F5F5F5'):
+        
+        """
+            Plots and saves hvplot line plot
+        """
+        
+        # Setting instance variable and title
         acp = 'Adjusted Close Price'
         title = title + ' ETF Adjusted Closing Prices'
         
+        # Saving line plot to variable
         line_plot = df.hvplot(
                             title=title,
                             ylabel=f'{acp} - USD',
@@ -45,11 +71,17 @@ class Plotter:
                                 bgcolor=bgcolor
                             )
         
+        # Saving image of plot
         hvplot.save(line_plot,self.get_image_path(title))
-        
+        # Returning plot
         return line_plot
         
     def bar(self, df, title):
+        
+        """
+            Plots and saves hvplot line bar
+        """
+        # Saving bar plot to variable
         bar_plot = df.hvplot.bar(
                             title=title,
                             ylabel=title,
@@ -64,12 +96,17 @@ class Plotter:
                                 fontsize=self.fontsize,
                                 bgcolor=self.bgcolor
                             )
-
+        # Saving image of plot
         hvplot.save(bar_plot,self.get_image_path(title))
-
+        # Returning plot
         return bar_plot
     
     def heatmap(self, df, title):
+        
+        """
+            Plots and saves seaborn heatmap
+        """
+        # Creating instance variables
         etf_price = 'ETF Price'
         title = title + f' {etf_price} Correlation'
         
